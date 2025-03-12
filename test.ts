1558fcdd5e8583f24e9b77a1017b3d68ad81a66c
Aleksandar Djuran <djuran89@gmail.com>
2025-03-12 22:42:08 +0100

    ewq

--- test.ts
+++ test.ts
@@ -1,10 +1,20 @@
-import Raspberry from "./class/Raspberry.ts";
+// import Raspberry from "./class/Raspberry.ts";
+import { Gpio } from "pigpio";
 
 const Reley = [14, 15, 18, 23];
 var arg = process.argv[2];
 console.log(arg);
 if (arg === "1") {
-	const ralay_1 = new Raspberry(23);
-	ralay_1.createGpio();
-	ralay_1.write(1);
+	// const ralay_1 = new Raspberry(23);
+	// ralay_1.createGpio();
+	// ralay_1.write(1);
+	const relay1 = new Gpio(23, { mode: Gpio.OUTPUT });
+	relay1.digitalWrite(1);
+}
+if (arg === "2") {
+	// const ralay_1 = new Raspberry(23);
+	// ralay_1.createGpio();
+	// ralay_1.write(1);
+	const relay1 = new Gpio(23, { mode: Gpio.OUTPUT });
+	relay1.digitalWrite(0);
 }
