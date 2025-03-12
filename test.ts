8e486d7042894668151d361141041240c9dbedbc
Aleksandar Djuran <djuran89@gmail.com>
2025-03-13 00:21:41 +0100

    tqew

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 import Raspberry from "./class/Raspberry.ts";
 
 const Reley = [14, 15, 18, 23];
-const ralay_1 = new Raspberry(14);
+const ralay_3 = new Raspberry(14);
 const ralay_2 = new Raspberry(15);
-const ralay_3 = new Raspberry(18);
+const ralay_1 = new Raspberry(18);
 const ralay_4 = new Raspberry(23);
 var arg = process.argv[2];
 
