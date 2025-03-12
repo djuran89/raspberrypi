33c78aef7578448ed74bb6a748b4d35832c05109
Aleksandar Djuran <djuran89@gmail.com>
2025-03-12 22:32:12 +0100

    test

--- test.ts
+++ test.ts
@@ -1,8 +1,8 @@
-const Raspberry = require("./class/Raspberry.ts");
+import Raspberry from "./class/Raspberry";
 
 const Reley = [14, 15, 18, 23];
 var arg = process.argv[2];
-
+console.log(arg);
 if (arg === "1") {
 	const ralay_1 = new Raspberry(14);
 	ralay_1.write(1);
