8a3942e63be0c943d601ab02a4f0773956953219
Aleksandar Djuran <djuran89@gmail.com>
2025-03-12 22:39:33 +0100

    c

--- test.ts
+++ test.ts
@@ -4,7 +4,7 @@ const Reley = [14, 15, 18, 23];
 var arg = process.argv[2];
 console.log(arg);
 if (arg === "1") {
-	const ralay_1 = new Raspberry(14);
+	const ralay_1 = new Raspberry(23);
 	ralay_1.createGpio();
 	ralay_1.write(1);
 }
